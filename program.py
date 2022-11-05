87085c46c323bdc387407498184f21350cf05289
John Ric Merque <johnricmer@gmail.com>
2022-11-05 11:58:22 +0800

    added remove and revert features

--- program.py
+++ program.py
@@ -8,7 +8,6 @@
 # - allow user to select item in the menu (check if valid)
 # - perform the selected option (you may prompt additional info to user when need)
 # - display the resulting values of the array
-
 Array = [2, 21, 7, 140, 80, 92, 12, 119, 350, 43]
 
 def main():
@@ -20,27 +19,12 @@ def main():
     print(" 4 -> Delete an element")
     print(" 5 -> Arrange in ascending order")
     print(" 6 -> Arrange in descending order")
-    methodIndex = input("What do you want to do? (1-6): ")
-    manipulateArray(methodIndex)
-
-def manipulateArray (method_index):
-    match method_index:
-        case "1":
-            appendValue = int(input("Enter the value you want to add: "))
-            Array.append(appendValue)
-            print("The value has been added")
-            print(f"This is the new Array: {Array}")
-            return
-        case "2":
-            insertIndex = int(input("Enter the index you want to insert to: "))
-            insertValue = int(input("Enter value:"))
-            Array.insert(insertIndex, insertValue)
-            print(f"This is the new Array: {Array}")
-            return
-        case _:
-            return "Something's wrong with the internet"
-
-def continueArrayManipulation():
+    print(" 7 -> Revert to default array")
+    print()
+    methodIndex = input("What do you want to do? (1-7): ")
+    manipulateArray(Array, methodIndex)
+    print()
+    
     continueCondition = input("Do you wish to continue manipulating the array? (y/n): ")
     if continueCondition == "y":
         main()
@@ -48,10 +32,36 @@ def continueArrayManipulation():
         print("Thank you user! Here's your final array:")
         print(Array)
 
-    
+
+def manipulateArray(array, method_index):
+    match method_index:
+        case "1":
+            appendElement = int(input("Enter the element you want to add: "))
+            array.append(appendElement)
+            print("The value has been added")
+            print(f"\nThis is the new Array: {array}")
+            return
+        case "2":
+            insertIndex = int(input("Enter the index you want to insert to: "))
+            insertElement = int(input("Enter element:"))
+            array.insert(insertIndex, insertElement)
+            print(f"\nThis is the new Array: {array}")
+        case "4":
+            removeElement = int(input("Enter element you want to delete:"))
+            array.remove(removeElement)
+            print(f"\nThis is the new Array: {array}")
+            return
+        case "7":
+            array = [2, 21, 7, 140, 80, 92, 12, 119, 350, 43]
+            print(f"\nThis is the new Array: {array}")
+            return
+        
+        case _:
+            return "Something's wrong with the internet"
+
 # program start
 main()
-continueArrayManipulation()
+
 
 
 
